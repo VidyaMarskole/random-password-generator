75fdfcb69ed5b3a69f70652db520e59476b0a3c9
Vidya Marskole <166975425+VidyaMarskole@users.noreply.github.com>
2024-10-09 17:28:34 +0530

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -2,6 +2,7 @@ import string
 import random
 
 
+#check length is greater than 7 or not
 def checkLen():
     while True:
         password_len=int(input('Enter the length of desired password: '))
@@ -10,11 +11,10 @@ def checkLen():
         else:
             print('Minimum length should be "8"')
             
-words= string.ascii_letters+string.digits+string.punctuation
+words= string.ascii_letters+string.digits+string.punctuation     #concate letters,digits and punctuation
 password_len=checkLen()
 password=''
 while(password_len>0):
     password+=random.choice(words)
     password_len-=1
 print('The desired password is ',password)
-    
